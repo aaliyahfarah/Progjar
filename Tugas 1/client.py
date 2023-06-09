a8e6fa4cf52cca1e74051202d496ed84c5ec7391
Aaliyah Farah Adibah <73101444+aaliyahfarah@users.noreply.github.com>
2023-06-09 15:40:03 +0700

    Update client.py

--- Tugas 1/client.py
+++ Tugas 1/client.py
@@ -1,6 +1,10 @@
 import sys
 import socket
 import logging
+import os
+
+BUFFER_SIZE = 4096
+SEPARATOR = "  "
 
 #set basic logging
 logging.basicConfig(level=logging.INFO)
@@ -10,19 +14,31 @@ try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
-    server_address = ('localhost', 10000)
+    server_address = ('172.16.16.101', 32444)
     logging.info(f"connecting to {server_address}")
     sock.connect(server_address)
 
     # Send data
-    message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
-    logging.info(f"sending {message}")
-    sock.sendall(message.encode())
+    #message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
+    message = "test.txt"
+    filesize = os.path.getsize(message)
+    
+    sock.send(f"{message}{filesize}".encode())
+    
+    #logging.info(f"sending {message}")
+    
+    with open(message,"rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            sock.sendall(bytes_read)
+    
     # Look for the response
     amount_received = 0
     amount_expected = len(message)
     while amount_received < amount_expected:
-        data = sock.recv(16)
+        data = sock.recv(4096)
         amount_received += len(data)
         logging.info(f"{data}")
         
