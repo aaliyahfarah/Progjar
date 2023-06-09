e838d741024192c15d7334dc4b03e9487b8b1d30
AALIYAH FARAH ADIBAH <aaliyahfarahadibah@gmail.com>
2023-06-09 15:38:10 +0700

    Tugas

---
+++ Tugas 1/server.py
@@ -0,0 +1,46 @@
+import sys
+import socket
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+try:
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(10)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+
+    # Bind the socket to the port
+    server_address = ('0.0.0.0', 10000) #--> gunakan 0.0.0.0 agar binding ke seluruh ip yang tersedia
+
+    logging.info(f"starting up on {server_address}")
+    sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1)
+    #1 = backlog, merupakan jumlah dari koneksi yang belum teraccept/dilayani yang bisa ditampung, diluar jumlah
+    #             tsb, koneks akan direfuse
+    while True:
+        # Wait for a connection
+        logging.info("waiting for a connection")
+        connection, client_address = sock.accept()
+        logging.info(f"connection from {client_address}")
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(32)
+            #32 -> merupakan buffersize, jumlah maksimum data yang bisa diterima sekaligus
+            #buffersize lebih baik diset dalam power of 2 contoh: 1024,32,4096
+            logging.info(f"received {data}")
+            if data:
+                logging.info("sending back data")
+                connection.sendall(data)
+            else:
+                #print >>sys.stderr, 'no more data from', client_address
+                #print(f"no more data from {client_address}")
+               break
+        # Clean up the connection
+        connection.close()
+except Exception as ee:
+    logging.log(f"ERROR: {str(ee)}")
+finally:
+    logging.log('closing')
+    sock.close()
---
+++ Tugas 2/server_thread.py
@@ -0,0 +1,47 @@
+from socket import *
+import socket
+import threading
+import logging
+import time
+import sys
+
+class ProcessTheClient(threading.Thread):
+	def __init__(self,connection,address):
+		self.connection = connection
+		self.address = address
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True:
+			data = self.connection.recv(32)
+			if data:
+				self.connection.sendall(data)
+			else:
+				break
+		self.connection.close()
+
+class Server(threading.Thread):
+	def __init__(self):
+		self.the_clients = []
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		threading.Thread.__init__(self)
+
+	def run(self):
+		self.my_socket.bind(('0.0.0.0',8889))
+		self.my_socket.listen(1)
+		while True:
+			self.connection, self.client_address = self.my_socket.accept()
+			logging.warning(f"connection from {self.client_address}")
+			
+			clt = ProcessTheClient(self.connection, self.client_address)
+			clt.start()
+			self.the_clients.append(clt)
+	
+
+def main():
+	svr = Server()
+	svr.start()
+
+if __name__=="__main__":
+	main()
+
