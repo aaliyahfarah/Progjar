5bc43b19213d19247fa66250aa5f9abd05284f88
Aaliyah Farah Adibah <73101444+aaliyahfarah@users.noreply.github.com>
2023-06-09 15:44:31 +0700

    Update client_1.py

--- Tugas 2/client_1.py
+++ Tugas 2/client_1.py
@@ -5,25 +5,28 @@ import logging
 
 
 def kirim_data():
+    nama='client_1'
+    logging.warning(f"nama {nama}")    
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     logging.warning("membuka socket")
 
-    server_address = ('localhost', 10000)
-    logging.warning(f"opening socket {server_address}")
+    server_address = ('172.16.16.103', 45000)
+    logging.warning(f"opening socket {server_address}\r\n")
     sock.connect(server_address)
-
+    
     try:
         # Send data
         message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
+        #message = 'TIME ' + rawmessage + '\r\n'
         logging.warning(f"[CLIENT] sending {message}")
-        sock.sendall(message.encode())
+        sock.sendall(message.encode('UTF-8'))
         # Look for the response
         amount_received = 0
         amount_expected = len(message)
-        while amount_received < amount_expected:
-            data = sock.recv(16)
-            amount_received += len(data)
-            logging.warning(f"[DITERIMA DARI SERVER] {data}")
+        #while amount_received < amount_expected:
+        data = sock.recv(1024).decode('UTF-8')
+        amount_received += len(data)
+        logging.warning(f"[DITERIMA DARI SERVER] {data}")
     finally:
         logging.warning("closing")
         sock.close()
