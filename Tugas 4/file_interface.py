a6b86b9c81782e89f6a0d52ca69789458fe9f2a0
Aaliyah Farah Adibah <73101444+aaliyahfarah@users.noreply.github.com>
2023-06-11 15:26:41 +0700

    Update file_interface.py

--- Tugas 4/file_interface.py
+++ Tugas 4/file_interface.py
@@ -25,7 +25,31 @@ class FileInterface:
             return dict(status='OK',data_namafile=filename,data_file=isifile)
         except Exception as e:
             return dict(status='ERROR',data=str(e))
-
+        
+        
+    def upload(self, param):
+        try:
+            filename = param[0] 
+            data = param[1]
+            simpan = open(filename, 'xb')
+            simpan.write(base64.b64decode(data))
+            simpan.close()
+        except IndexError:
+            return dict(status='ERROR',data='some parameters are missing')
+        except FileExistsError:
+            return dict(status='ERROR', data = 'file name exsist')
+        return dict(status="OK", data='upload berhasil');
+    
+    
+    def delete(self,param):
+        try:
+            filename = param[0]
+            os.remove(filename)
+        except IndexError:
+            return dict(status='ERROR',data='some parameters are missing')
+        except FileNotFoundError:
+            return dict(status='ERROR',data='file not found')
+        return dict(status='OK',data="file deleted successfully")
 
 
 if __name__=='__main__':
